31ec9388e5b1731175bc0848eaa308b28c9f99a9
mingz <305603665@qq.com>
2019-06-01 12:37:48 +0800

    dd

--- services/insert_data.go
+++ services/insert_data.go
@@ -62,7 +62,7 @@ var userDataC *dao.UserDataC
 //}
 
 func InsertUserData(waitGroup *sync.WaitGroup, tokens chan<- int, index int) {
-	log.Println("InsertUserData..")
+	//log.Println("InsertUserData..")
 
 	defer waitGroup.Done()
 
@@ -75,8 +75,8 @@ func InsertUserData(waitGroup *sync.WaitGroup, tokens chan<- int, index int) {
 
 		interval := endTime.Sub(startTime)
 
-		fmt.Println("interval: ", interval)
-
+		fmt.Println("interval: ", interval, int(interval))
+		log.Println("log interval: ", interval, int(interval))
 		tokens <- 1
 	}()
 
@@ -89,14 +89,14 @@ func InsertUserData(waitGroup *sync.WaitGroup, tokens chan<- int, index int) {
 
 	err := userDataC.Insert(data)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("err: ", err)
 	}
 
 }
 
 func InsertAllUserData(count int, concurrentCount int) {
 	//fmt.Println("InsertAllUserData...", count, concurrentCount)
-	//log.Println("InsertAllUserData....")
+	log.Println("InsertAllUserData....")
 
 	tokens := make(chan int, concurrentCount+10)
 
@@ -112,26 +112,35 @@ func InsertAllUserData(count int, concurrentCount int) {
 
 	i = 0
 
+	var breakFor = false
+
 	for {
 		//log.Println("for...count...")
 		select {
 		case <-tokens:
 			// 拿到一个token，
-			waitGroup.Add(count)
+			waitGroup.Add(1)
 			go InsertUserData(&waitGroup, tokens, i)
 
 			i++
 
 			if i >= count {
+				log.Println("i > count...")
+				breakFor = true
 				break
 			}
 
-			continue
-
 		case <-time.After(time.Microsecond * 1):
-			continue
+			break
+		default:
+			break
 
 		}
+
+		if breakFor {
+			break
+		}
+
 	}
 
 	//w:= make(chan int)
@@ -141,6 +150,8 @@ func InsertAllUserData(count int, concurrentCount int) {
 	//
 	//}
 
+	log.Println("befor wait..")
 	waitGroup.Wait()
 
+	log.Println("InsertAllUserData....end...")
 }
